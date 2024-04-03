0989f622c853a5e7ac6adf70ddd9137842733f5f
James Kamau <jameshmwangiy@gmail.com>
2024-04-03 05:06:54 +0300

    Update api.php

--- utibu_health_pharmacy_laravel/routes/api.php
+++ utibu_health_pharmacy_laravel/routes/api.php
@@ -8,23 +8,28 @@ use App\Http\Controllers\UsersController;
 
 use App\Http\Controllers\InventoryController;
 
-// List inventory entries
-Route::get('/inventory', [InventoryController::class, 'index']);
-
-// Create a new inventory entry
-Route::post('/inventory', [InventoryController::class, 'store']);
-
-// Update an inventory entry
-Route::put('/inventory/{inv_id}', [InventoryController::class, 'update']);
-
-// Place an order
-Route::post('/inventory/{inv_id}/order/{quantity}', [InventoryController::class, 'placeOrder']);
-
-Route::post('/login', [UsersController::class, 'login']);
-
-Route::post('/register', [UsersController::class, 'register']);
 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+Route::middleware(['auth:sanctum', 'api'])->group(function() {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+     // Only admin users can create a new inventory entry
+     Route::post('/inventory', [InventoryController::class, 'store'])
+     ->middleware('can:admin-access');
+
+// Only admin users can update an inventory entry
+Route::put('/inventory/{inv_id}', [InventoryController::class, 'update'])
+     ->middleware('can:admin-access');
+
+    // Update an inventory entry
+    Route::put('/inventory/{inv_id}', [InventoryController::class, 'update']);
+
+    // Place an order for an inventory item
+    Route::post('/inventory/{inv_id}/order/{quantity}', [InventoryController::class, 'placeOrder']);
+});
+
